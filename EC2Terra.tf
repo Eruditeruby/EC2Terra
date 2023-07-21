fdc0b481732f9b3d0898b2721e37b8f0bebde99c
Eruditeruby <rubisha.saini@gmail.com>
2023-07-21 09:31:02 +0000

    commit-2

---
+++ EC2Terra.tf
@@ -0,0 +1,59 @@
+iprovider "aws" {
+  region = "eu-central-1"  # Replace with your desired AWS region
+}
+
+
+resource "aws_security_group" "project-EC2Terra" {
+  name = "EC2Terra"
+  description = "EC2Terra Security Group"
+
+  // To Allow SSH Transport
+  ingress {
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // To Allow Port 80 Transport
+  ingress {
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_instance" "project-EC2Terra" {
+  ami = "ami-07ce6ac5ac8a0ee6f"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.myseckey.id
+  tags = {
+    Name = "EC2Terra-instance"
+    }
+#   user_data = <<-EOF
+#   #!/bin/bash
+
+#   #Install Minikube
+    #  curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+    #  sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+#   # Start Minikube
+#     minikube start
+#     EOF   
+# }
+
+resource "aws_key_pair" "myseckey" {
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCRT94sjRL1vXJnf/0yWL6tBWz/GSv9kIyC6aUloznlA7hv7vp8EihxKPLGm6HsjZH4UI7GSAd6RC0TSNZqzXKWaB745A+Nz2Kq+NAq8+KJunSIvh1N5ze8zflY8VLSdtO0lUdAdxay7FsTD7pYNMQfLrUuPj0c2SwBJUn5GT40d8IsRobuuIx4e1GdEJWVMhmhUDN7nRq39Tzvul7MCYV/Ohud54cJL8amiw4mkJgmUBaRmhoNIsDTdfkAFdLPkwR5y1Zgpj/JBiJCiKBjzAV3PETKTD91WGpq4kl9FMv7eqTYZOlpVOZOL2iXxTr2d0nYfsp1OvIVhDXm3ZSqJ3Dp"
+}
